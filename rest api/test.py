ea5f980bbd0570ba49dc1456fd77b546fd3f4dad
Lenovo <wanumair1209@gmail.com>
2024-09-13 11:28:59 +0800

    done setting up the api, need to do task given on thursday 12/9

---
+++ rest api/test.py
@@ -0,0 +1,78 @@
+from flask import Flask, jsonify, abort
+from flask_restful import Api, Resource, reqparse
+from geopy.geocoders import Nominatim
+import rasterio as rio
+import glob
+
+
+def address_to_latlon(location):
+    try:
+        loc = Nominatim(user_agent='test_1')
+        location = loc.geocode(location)
+        data = location.latitude, location.longitude
+    except AttributeError:
+        data = None, None
+
+    return data
+
+def raster_read(latitude, longitude):
+    # add looping for files end with tif, return raster path and pixel value might have many raster file
+    files = glob.glob('./*.tif')
+    for file in files:
+        # open the raster file
+        raster_data = rio.open(file)
+        # read first band
+        raster_value = raster_data.read(1)
+        rowIndex, colIndex = raster_data.index(longitude, latitude)
+        try:
+            pixel_value = raster_value[rowIndex, colIndex]
+        except IndexError:
+            pixel_value = None
+        return file, pixel_value
+
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+input_post_args = reqparse.RequestParser()
+# input_post_args.add_argument("raster_path", type=str, help='raster file is needed', required=True)
+input_post_args.add_argument("address", type=str, help='address is needed', required=True)
+
+
+class Output(Resource):
+    def get(self):
+        return {'data': {'message': 'no output for this method'}}
+
+    def post(self):
+        args = input_post_args.parse_args()
+        address = args['address']
+        # raster_path = args['raster_path']
+        latitude, longitude = address_to_latlon(location=address)
+        if latitude is None:
+            abort(404, description="Address is unrecognizable")
+        #catch error 2 AttributeError: 'NoneType' object has no attribute 'latitude'
+        file_name, pixel_value = raster_read(latitude=latitude, longitude=longitude)
+        #catch error 1 index IndexError: index -5048 is out of bounds for axis 0 with size 3601
+        if pixel_value is None:
+            abort(404, description="address is out of range for raster image uploaded")
+        output = {'data': {'address': address,
+                           # 'raster_path': raster_path,
+                           'latitude': latitude,
+                           'longitude': longitude,
+                           'pixel_value': int(pixel_value),
+                           'raster file': file_name}}
+
+        return jsonify(output)
+
+api.add_resource(Output, '/output')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+### might encounter problem
+#1. nominatim not very suitable for product base (cant do many request, not very reliable and cannot locate many addresses esp duopharma).
+
